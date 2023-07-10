f41d6eba1379ab89ea4420948312b6e929ddc5a8
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-10 12:31:39 +0530

    arthmatic operations

---
+++ 4_print2.py
@@ -0,0 +1,16 @@
+n=5
+m=2
+ans1=n+m
+print("addition of two is : ",ans1)
+ans2=n-m
+print("subtraction of two is : ",ans2)
+ans3=n*m
+print("multiplication of two is : ",ans3)
+ans4=n/m
+print("division of two is : ",ans4)
+ans5=n**m
+print("exponent of two is : ",ans5)
+ans6=n//m
+print("divison only int number of two is : ",ans6)
+ans7=n%m
+print("remender of two is : ",ans7)
