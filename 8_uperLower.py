e079e1d3fd76cd25da377092a769f05e8b05d8ba
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-11 18:46:21 +0530

    inbuilt function in python

---
+++ 8_uperLower.py
@@ -0,0 +1,15 @@
+n="#### Abhishek #####"
+print(len(n))
+print(n.upper())
+print(n.lower())
+print(n.lstrip("#"))
+print(n.replace("####","Rajput"))
+print(n.split(" "))
+print(n.count("#"))
+print(n.find("b"))
+print(n.index("i"))
+
+# it give true or false
+print(n.isalpha())
+print(n.islower())
+print(n.isspace())
