4634c6ed55c3b079749f4861fcdb6b699ec19a69
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-11 11:30:43 +0530

    Slicin in string

---
+++ 7_string2.py
@@ -0,0 +1,15 @@
+
+n="My name is Abhishek Rajput"
+# for ch in n :
+#     print (ch)
+
+# slicing in string 
+
+print (n[ : ])
+print (n[ :9])
+print (n[1 : 13 : 2])
+print (n[ :-3])
+print (n[-4:-1])
+print(n[-1:-4])
+
+
