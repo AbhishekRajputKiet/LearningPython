03edf25d26e217ce91cdf10fb173c3a9decc1286
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-13 00:09:08 +0530

    Function in python

---
+++ 15_function.py
@@ -0,0 +1,8 @@
+def sum(a,b):
+    print (a+b)
+n=int(input("Loop value : "))
+for i in range (1,n+1):
+    a=int(input("Enter value 1st : "))
+    b=int(input("Enter value 2nd : "))
+    print("Sum of given number is ",sum(a,b))
+
