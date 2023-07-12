573876e03a038d4a4abf35a0dec8f6aef762cf58
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-12 14:28:16 +0530

    If else operater

---
+++ 9_if_else.py
@@ -0,0 +1,17 @@
+a=int(input(("Enter a number : ")))
+print("Your age is : ",a)
+if a>18:
+    print ("You can driver.")
+elif a==18 :
+    print("You can boat.")
+else:
+    print("You can not drive.")
+#  conditional operater
+#  <,>,<=,>=,==,!=
+print (a>18)
+print(a<18)
+print (a>=18)
+print(a<=18)
+print(a!=18)
+print(a==18)
+
