0e311ac3dab446286d3a3ed44ca34c5d63f12e9a
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-12 20:09:50 +0530

    For loop in python

---
+++ 12_forloop.py
@@ -0,0 +1,13 @@
+name = 'Abhishek'
+for i in name :
+    print(i,end=" ")
+print()
+
+colors={"Red","Green","Orange","Pink"}
+for i in colors :
+    print(i)
+    for j in i:
+        print(j,end=" ")
+
+for i in range(1,51):
+    print(i,end=" ")
