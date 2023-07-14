0e144888d8409560066b87e706627d7280fecfec
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-14 16:48:51 +0530

    Operation on tuples

---
+++ 20_opeOfTuples.py
@@ -0,0 +1,13 @@
+name = ("abhi","akash","ankit","anurag","rajput")
+name2=list(name)
+name2[1]="kumar"
+name=tuple(name2)
+print(name)
+
+secA=("abhi","akash","ankit","anurag","rajput")
+secB=("chirag","bablu","bumi","dinash")
+secc=secA+secB
+print (secc)
+
+n= (23,57,34,2,2,23,4,3,2,12,23)
+print(n.count(23))
