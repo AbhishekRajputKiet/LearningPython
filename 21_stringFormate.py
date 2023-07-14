c878ba3f2d21b5c8c4eadcc0cd893332138410b4
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-14 19:27:04 +0530

    String formationg in python

---
+++ 21_stringFormate.py
@@ -0,0 +1,11 @@
+name="My name is {} and ser name is {}"
+first="Abhishek"
+second="Rajput"
+print(name.format(first,second))
+
+print(f"My name is {first} and sername is {second}")
+print(f"My name is {{first}} and sername is {{second}}")
+
+print("My name is {} and sername is {}".format(first,second))
+prise=29.03328
+print(f"Prise is {prise : .2f}")
