037e178faca8bdb2706bebf863fcc5e0c7b63939
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-16 12:35:47 +0530

    For or While loop with else condition

---
+++ 29_whilewith_else.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    print(i)
+else :
+    print("I is overlode")
+print()
+for j in range(6):
+    print(j)
+    if j==4:
+        break
+else :
+    print("J is over")
+print()
+i=0
+while i<7:
+    print (i)
+    i=i+1
+    # if i==3:
+    #     break
+else:
+    print("while is over")
+
+
