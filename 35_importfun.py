8d0fd7e440c1443036382656c9b17adcb7887ae0
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-18 12:20:31 +0530

    Import function working

---
+++ 35_importfun.py
@@ -0,0 +1,21 @@
+import math
+x=34.2948
+print(math.floor(x))
+print(math.sqrt(x))
+
+from math import sqrt, pi
+result=sqrt(9)*pi
+print(result)
+
+from math import sqrt as s,pi as p
+print(s(9)*p)
+
+
+import math as m
+print(m.sqrt(9)*m.pi)
+
+import math
+print(dir(math))
+
+
+
