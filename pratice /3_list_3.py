3b2030af679f9378b2c2c6d89e85efeffdce5c47
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-18 13:06:56 +0530

    Length of list

---
+++ pratice /3_list_3.py
@@ -0,0 +1,10 @@
+#  function for find length of string
+
+def swapusingslicing(nums,c):
+    c=0
+    for i in  nums:
+        c=c+1
+    return c
+nums=[23, 65, 19, 90]
+c=0
+print(swapusingslicing(nums,c))
