2255ed7c74aebc4cb6bd99c6048e744d226fa1df
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-20 16:07:54 +0530

    __name__   means

--- abhi.py
+++ abhi.py
@@ -1,5 +1,5 @@
 def name():
     print("Welcome in my room.")
-if __name__=="__main__":     # this tels which file is run 
+if __name__=="__main__":     # this tels which file is run current or import file 
     name()                   # its seperate output in which this file is import
     
