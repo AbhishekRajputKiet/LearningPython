233520130432ef00a14bc7d90927f1130ea71334
Abhishek Rajput <118709229+AbhishekRajputKiet@users.noreply.github.com>
2023-09-15 22:20:23 +0530

    Map Filter Reduse function

---
+++ 42_MapFilterReduse.py
@@ -0,0 +1,30 @@
+
+# MAP
+def cube(x):
+    return x*x*x
+print(cube(3))
+
+
+
+l=[2,4,5,1,6]
+# newl=[]
+# for i in l:
+#     newl.append(cube(i))
+
+newl = list(map(cube,l))
+print(newl)
+
+#  FILTER
+def filter_function(a):
+    return a>2
+newlist = list(filter(filter_function,l))
+print(newlist)
+
+# REDUCE
+
+from functools import reduce
+num = [1 ,3,4,5,6]
+def mysum(x,y):
+    return x+y
+sum = reduce(mysum,num)
+print(sum) 
