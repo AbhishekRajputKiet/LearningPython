246316afeb5086b7436517dc4dce34e921b3c94e
Abhishek Rajput <118709229+AbhishekRajputKiet@users.noreply.github.com>
2023-09-20 18:37:45 +0530

    git push origin master

---
+++ 50_ClassMethod.py
@@ -0,0 +1,15 @@
+class Employee :
+    company="TCS"
+    def show(self):
+        print(f"Name of company is {self.company} and my name is {self.name}")
+    @classmethod
+    # class decorater is use to pass the first element of function as a class 
+    def changecomp(cls,newcompany):
+        cls.company=newcompany
+e1=Employee()
+e1.name="Abhishek Rajput"
+e1.show()
+e1.changecomp("Apple")
+
+e1.show()
+print(Employee.company)
