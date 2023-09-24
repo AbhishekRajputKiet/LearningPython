0ca35bd2a6147db228ab2f57e08775b8d78ff497
Abhishek Rajput <118709229+AbhishekRajputKiet@users.noreply.github.com>
2023-09-24 22:10:44 +0530

    Dender Method

---
+++ 55_DenderMethod.py
@@ -0,0 +1,10 @@
+# Dender method or magic method are use under class for specific task
+class Employee:
+    name="Abhishek"
+    length=len(name)
+
+     
+a = Employee()
+print(a.name)
+# print(len(a))
+
